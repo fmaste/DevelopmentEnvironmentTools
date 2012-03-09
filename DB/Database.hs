3cb8b278cd2b213cd68122f6e3ab6bfa2e0ba6e4
fmaste <fmaste@users.noreply.github.com>
2012-03-09 18:14:56 -0300

    Server abstraction

--- DB/Database.hs
+++ DB/Database.hs
@@ -1,4 +1,9 @@
 module Database (
+	ServerHost,
+	Server (Server),
+	User,
+	Password,
+	ServerDatabase (ServerDatabase),
         DatabaseName,
         Database (Database),
         TableName,
@@ -11,18 +16,35 @@ module Database (
 
 -------------------------------------------------------------------------------
 
+-- A server has a host and databases associated.
+type ServerHost = String
+
+data Server = Server ServerHost [ServerDatabase]
+
+-- The server databases have a user and a password.
+type User = String
+
+type Password = String
+
+data ServerDatabase = ServerDatabase Database User Password
+
+-- The database has a name and tables.
 type DatabaseName = String
 
 data Database = Database DatabaseName [Table]
 
+-- Every table has a name a fields.
 type TableName = String
 
 data Table = Table TableName [Field]
 
+-- A field has a name and a type.
 type FieldName = String
 
 data Field = Field FieldName FieldType
 
+-- The field types are bool, int and string. 
+-- Can be not null or nullable.
 data FieldType = FieldType ValueType | MaybeFieldType ValueType
 
 data ValueType = ValueBool | ValueInt | ValueString
