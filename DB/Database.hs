2eaf9dbe6d7e2dd083121cf64c78b5f7ddab37ef
fmaste <fmaste@users.noreply.github.com>
2012-03-22 11:01:46 -0300

    Add all the deriving

--- DB/Database.hs
+++ DB/Database.hs
@@ -26,26 +26,33 @@ module Database (
 type DatabaseName = String
 
 data Database = Database DatabaseName [Table]
+	deriving (Show, Read, Eq, Ord)
 
 -- Every table has a name and fields.
 -- This is the name as used for the SQL queries.
 type TableName = String
 
 data Table = Table TableName [Field]
+	deriving (Show, Read, Eq, Ord)
 
 -- A field has a name and a type.
 -- This is the name as used for the SQL queries.
 type FieldName = String
 
 data Field = Field FieldName FieldType
+	deriving (Show, Read, Eq, Ord)
 
 -- The field types are bool, int and string. 
 -- Can be not null or nullable.
 data FieldType = FieldType Nullable ValueType
+	deriving (Show, Read, Eq, Ord)
 
 data Nullable = Null | NotNull
+	deriving (Show, Read, Eq, Ord)
 
 data ValueType = ValueBool | ValueInt | ValueString
+	deriving (Show, Read, Eq, Ord)
+
 -- A foreign key is a relation between two fields.
 data FK = FK Field Field
 	deriving (Show, Read, Eq, Ord)
